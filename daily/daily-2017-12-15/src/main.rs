14e100e853b01ce833b03944d6db92e09b7a6fae
dishmint <fzaman93@protonmail.com>
2022-09-02 20:08:22 -0400

    Implemented simple version of daily_2017_12_15

--- daily/daily-2017-12-15/src/main.rs
+++ daily/daily-2017-12-15/src/main.rs
@@ -1,21 +1,36 @@
 use nannou::prelude::*;
 
+mod grid;
+
+use crate::grid::Grid;
 fn main() {
     nannou::app(model)
         .update(update)
+        .size(400,400)
         .simple_window(view)
         .run();
 }
 
-struct Model {}
-
-fn model(_app: &App) -> Model {
-    Model {}
+struct Model {
+    grid: Grid
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {
+fn model(app: &App) -> Model {
+    let win = app.window_rect();
+    let grid = Grid::new(win, 10,10);
+    Model {grid}
 }
 
-fn view(_app: &App, _model: &Model, frame: Frame){
-    frame.clear(PURPLE);
+fn update(app: &App, model: &mut Model, _update: Update) {
+    let win = app.window_rect();
+    model.grid.update(win);
+}
+
+fn view(app: &App, model: &Model, frame: Frame){
+    let draw = app.draw();
+    let win = app.window_rect();
+    draw.background().color(BLACK);
+
+    model.grid.view(win, &draw);
+    draw.to_frame(app, &frame).unwrap();
 }
